e4e60426da3a3df29b806ac25d13bcd0e2df34e3
Martinez1703 <164842551+Martinez1703@users.noreply.github.com>
2025-05-20 14:37:25 +0300

    Add files via upload

---
+++ LabaKg - Powerhnost/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace LabaKg3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button9 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MV Boli", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(820, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Нарисовать оси";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(820, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(244, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Нарисовать фигуры";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(861, 134);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 34);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(9, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(706, 723);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(738, 528);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(376, 67);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Масштаб";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(760, 277);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(354, 73);
+            this.button16.TabIndex = 30;
+            this.button16.Text = "Поворот по Y";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(760, 198);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(354, 73);
+            this.button4.TabIndex = 31;
+            this.button4.Text = "Поворот по X";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(760, 356);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(354, 73);
+            this.button5.TabIndex = 32;
+            this.button5.Text = "Поворот по Z";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(738, 612);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(388, 70);
+            this.button6.TabIndex = 33;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1165, 747);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Преобразрвания на плоскости";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ LabaKg - Powerhnost/Form1.cs
@@ -0,0 +1,490 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+using static System.Windows.Forms.AxHost;
+
+namespace LabaKg3
+{
+    public partial class Form1 : Form
+    {
+        bool f = true;
+        int k, l, m; // элементы матрицы сдвига
+        float[,] kvSohran = new float[8, 4]; // Уменьшил размер, так как originalKv имеет 8 строк
+        float[,] kv = new float[8, 4]; // матрица тела велосипеда (8 вершин)
+        float[,] osi = new float[4, 3]; // матрица координат осей
+        float[,] matr_sdv = new float[4, 4]; //матрица преобразования
+        private double rotationAngle = 0;
+        private double wheelRotationAngle = 0; // угол поворота колес
+        private float[,] originalKv = new float[8, 4]; // Для хранения исходных координат (float)
+        private const int wheelRadius = 48; // радиус колес
+        private const int spokeCount = 8; // количество спиц в колесе
+        private float scale = 1.0f;       // Масштаб
+        private double rotationAngleY = 0; // Угол поворота вокруг оси Y в радианах
+        private double rotationAngleX = 0; // Угол вращения вокруг оси X
+
+        private double rotationAngleZ = 0; // Угол вращения вокруг оси Z
+        public Form1()
+        {
+            InitializeComponent();
+            Init_kvadrat(); // Инициализируем фигуру один раз при запуске
+            Array.Copy(kv, originalKv, kv.Length); // Сохраняем исходное состояние
+        }
+        //обновление для осей
+        private float[,] CalculateFunctionValues(int stepsX, int stepsY)
+        {
+            float[,] zValues = new float[stepsX, stepsY];
+            float xMin = -3f, xMax = 3f;
+            float yMin = -3f, yMax = 3f;
+
+            for (int i = 0; i < stepsX; i++)
+            {
+                float x = xMin + (xMax - xMin) * i / (stepsX - 1);
+                for (int j = 0; j < stepsY; j++)
+                {
+                    float y = yMin + (yMax - yMin) * j / (stepsY - 1);
+                    zValues[i, j] = (float)Math.Exp(Math.Sin(y) - x * x);
+                }
+            }
+            return zValues;
+        }
+        
+        private void DrawStaticAxes()
+        {
+            // Оси будут рисоваться относительно центра pictureBox
+            int centerX = pictureBox1.Width / 2;
+            int centerY = pictureBox1.Height / 2;
+
+            Pen axisPen = new Pen(Color.Red, 1);
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+
+            // Рисуем горизонтальную ось X
+            g.DrawLine(axisPen, 0, centerY, pictureBox1.Width, centerY);
+
+            // Рисуем вертикальную ось Y
+            g.DrawLine(axisPen, centerX, 0, centerX, pictureBox1.Height);
+
+            g.Dispose();
+            axisPen.Dispose();
+        }
+
+       
+        private void ClearDrawing()
+        {
+            pictureBox1.Image = null;
+            pictureBox1.Refresh();
+        }
+        private void ResetFigure()
+        {
+            Array.Copy(originalKv, kv, originalKv.Length);
+            Draw_Kv(); // Перерисовываем фигуру в исходном положении
+        }
+        private void Init_kvadrat()
+        {
+
+            kv[0, 0] = 0; kv[0, 1] = 0; kv[0, 2] = 0; kv[0, 3] = 1;
+            kv[1, 0] = 100; kv[1, 1] = 0; kv[1, 2] = 0; kv[1, 3] = 1;
+            kv[2, 0] = 100; kv[2, 1] = 100; kv[2, 2] = 0; kv[2, 3] = 1;
+            kv[3, 0] = 0; kv[3, 1] = 100; kv[3, 2] = 0; kv[3, 3] = 1;
+            //верх
+            kv[4, 0] = 0; kv[4, 1] = 100; kv[4, 2] = 100; kv[4, 3] = 1;
+            kv[5, 0] = 100; kv[5, 1] = 100; kv[5, 2] = 100; kv[5, 3] = 1;
+            kv[6, 0] = 100; kv[6, 1] = 0; kv[6, 2] = 100; kv[6, 3] = 1;
+            kv[7, 0] = 0; kv[7, 1] = 0; kv[7, 2] = 100; kv[7, 3] = 1;
+
+            // Сохраняем исходные координаты
+            Array.Copy(kv, originalKv, kv.Length);
+        }
+        private float rotationX = (float)(Math.PI / 2); // 90° в радианах
+        private void Draw_Kv()
+        {
+            ClearDrawing();
+
+            // Создаем матрицу поворота
+            float[,] rotationX = CreateRotationXMatrix(rotationAngleX);
+            float[,] rotationY = CreateRotationYMatrix(rotationAngleY);
+            float[,] rotationZ = CreateRotationZMatrix(rotationAngleZ);
+
+            // Комбинируем повороты (порядок важен!)
+            float[,] rotationMatrix = Multiply_matr(rotationX, rotationY);
+            rotationMatrix = Multiply_matr(rotationMatrix, rotationZ);
+
+            // Применяем поворот к каждой точке функции
+            float[,] zValues = CalculateFunctionValues(30, 30);
+            int stepsX = zValues.GetLength(0);
+            int stepsY = zValues.GetLength(1);
+
+            int centerX = pictureBox1.Width / 2;
+            int centerY = pictureBox1.Height / 2;
+
+            using (Graphics g = Graphics.FromHwnd(pictureBox1.Handle))
+            using (Pen pen = new Pen(Color.Blue, 1))
+            {
+                // Преобразуем и рисуем все точки
+                PointF[,] transformedPoints = new PointF[stepsX, stepsY];
+
+                for (int i = 0; i < stepsX; i++)
+                {
+                    for (int j = 0; j < stepsY; j++)
+                    {
+                        float x = -3f + 6f * i / (stepsX - 1);
+                        float y = -3f + 6f * j / (stepsY - 1);
+                        float z = zValues[i, j];
+
+                        // Применяем поворот
+                        float[] point = { x, y, z, 1 };
+                        float[] rotatedPoint = new float[4];
+
+                        for (int k = 0; k < 4; k++)
+                        {
+                            rotatedPoint[k] = 0;
+                            for (int l = 0; l < 4; l++)
+                            {
+                                rotatedPoint[k] += point[l] * rotationMatrix[l, k];
+                            }
+                        }
+
+                        // Проецируем на 2D с учетом масштаба и центра
+                        transformedPoints[i, j] = new PointF(
+                            centerX + rotatedPoint[0] * scale * 20,
+                            centerY - rotatedPoint[1] * scale * 20
+                        );
+                    }
+                }
+
+                // Рисуем линии вдоль X (вертикальные линии)
+                for (int j = 0; j < stepsY; j++)
+                {
+                    PointF[] linePoints = new PointF[stepsX];
+                    for (int i = 0; i < stepsX; i++)
+                    {
+                        linePoints[i] = transformedPoints[i, j];
+                    }
+                    g.DrawLines(pen, linePoints);
+                }
+
+                // Рисуем линии вдоль Y (горизонтальные линии)
+                for (int i = 0; i < stepsX; i++)
+                {
+                    PointF[] linePoints = new PointF[stepsY];
+                    for (int j = 0; j < stepsY; j++)
+                    {
+                        linePoints[j] = transformedPoints[i, j];
+                    }
+                    g.DrawLines(pen, linePoints);
+                }
+            }
+
+            
+        }
+        private float[,] ApplyTransformations(float[,] zValues)
+        {
+            int stepsX = zValues.GetLength(0);
+            int stepsY = zValues.GetLength(1);
+            float[,] transformed = new float[stepsX, stepsY];
+
+            // Преобразуем 2D-массив в 3D-точки (x, y, z)
+            float xMin = -3f, xMax = 3f;
+            float yMin = -3f, yMax = 3f;
+
+            for (int i = 0; i < stepsX; i++)
+            {
+                float x = xMin + (xMax - xMin) * i / (stepsX - 1);
+                for (int j = 0; j < stepsY; j++)
+                {
+                    float y = yMin + (yMax - yMin) * j / (stepsY - 1);
+                    float z = zValues[i, j];
+
+                    // Применяем преобразования (поворот, сдвиг, масштаб)
+                    float newX = x;
+                    float newY = y;
+                    float newZ = z;
+
+                    // 1. Поворот вокруг осей (если нужно)
+                    if (rotationAngle != 0)
+                    {
+                        //  поворот вокруг оси Y
+                        double cosA = Math.Cos(rotationAngle);
+                        double sinA = Math.Sin(rotationAngle);
+                        newX = (float)(x * cosA - z * sinA);
+                        newZ = (float)(x * sinA + z * cosA);
+                    }
+
+                    // 2. Сдвиг (k, l, m)
+                    newX += k;
+                    newY += l;
+                    newZ += m;
+
+                    // 3. Масштабирование (если нужно)
+                    newX *= scale;
+                    newY *= scale;
+                    newZ *= scale;
+
+                    transformed[i, j] = newZ; // Возвращаем только Z (X и Y используются для позиционирования)
+                }
+            }
+
+            return transformed;
+        }
+        private void Init_matr_preob(int k1, int l1, int m1)
+        {
+            matr_sdv[0, 0] = 1; matr_sdv[0, 1] = 0; matr_sdv[0, 2] = 0; matr_sdv[0, 3] = 0;
+            matr_sdv[1, 0] = 0; matr_sdv[1, 1] = 1; matr_sdv[1, 2] = 0; matr_sdv[1, 3] = 0;
+            matr_sdv[2, 0] = 0; matr_sdv[2, 1] = 0; matr_sdv[2, 2] = 1; matr_sdv[2, 3] = 0;
+            matr_sdv[3, 0] = k1; matr_sdv[3, 1] = l1; matr_sdv[3, 2] = m1; matr_sdv[3, 3] = 1;
+        }
+
+        private void Init_osi()
+        {
+            osi[0, 0] = -200; osi[0, 1] = 0; osi[0, 2] = 1;
+            osi[1, 0] = 200; osi[1, 1] = 0; osi[1, 2] = 1;
+            osi[2, 0] = 0; osi[2, 1] = 200; osi[2, 2] = 1;
+            osi[3, 0] = 0; osi[3, 1] = -200; osi[3, 2] = 1;
+        }
+        private float[,] Multiply_matr(float[,] a, float[,] b)
+        {
+            int n = a.GetLength(0);
+            int m = b.GetLength(1);
+            int m_a = a.GetLength(1);
+            if (m_a != b.GetLength(0)) throw new Exception("Матрицы нельзя перемножить!");
+            float[,] r = new float[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    r[i, j] = 0;
+                    for (int ii = 0; ii < m_a; ii++)
+                    {
+                        r[i, j] += a[i, ii] * b[ii, j];
+                    }
+                }
+            }
+            return r;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //середина pictureBox
+            k = pictureBox1.Width / 2;
+            l = pictureBox1.Height / 2;
+            Draw_Kv();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            k = pictureBox1.Width / 2;
+            l = pictureBox1.Height / 2;
+            DrawStaticAxes();
+        }
+        //Таемер для сдвига
+
+
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            ClearDrawing();
+            DrawStaticAxes();
+            l -= 5; //изменение соответствующего элемента матрицы сдвига
+            Draw_Kv(); //вывод квадратика
+        }
+        //Масштабирование  фигуры на плоскости
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Поворот фигуры
+       
+        
+        // Метод для умножения матриц 4x4
+        private double[,] MultiplyMatrices(double[,] a, double[,] b)
+        {
+            double[,] result = new double[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    result[i, j] = 0;
+                    for (int k = 0; k < 4; k++)
+                    {
+                        result[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            return result;
+
+        }
+
+
+        //Отражение фигуры относительно Х
+        private void button11_Click(object sender, EventArgs e)
+        {
+           
+            // Матрица отражения относительно оси X
+            float[,] reflectX = new float[4, 4]
+            {
+                { 1, 0, 0, 0 },
+                { 0, -1, 0, 0 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 1 }
+            };
+
+            // Применяем отражение к текущим координатам
+            float[,] temp = Multiply_matr(kv, reflectX);
+
+            // Обновляем текущие координаты фигуры
+            for (int i = 0; i < 8; i++)
+            {
+                kv[i, 0] = temp[i, 0];
+                kv[i, 1] = temp[i, 1];
+                kv[i, 2] = temp[i, 2];
+                kv[i, 3] = temp[i, 3];
+            }
+
+            // Рисуем отраженную фигуру с учетом текущего сдвига
+            Draw_Kv();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            
+            // Матрица отражения относительно оси Y
+            float[,] reflectY = new float[4, 4]
+            {
+                { -1, 0, 0, 0 },
+                { 0, 1, 0, 0 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 1 }
+            };
+
+            // Применяем отражение к текущим координатам
+            float[,] temp = Multiply_matr(kv, reflectY);
+
+            // Обновляем текущие координаты фигуры
+            for (int i = 0; i < 8; i++)
+            {
+                kv[i, 0] = temp[i, 0];
+                kv[i, 1] = temp[i, 1];
+                kv[i, 2] = temp[i, 2];
+                kv[i, 3] = temp[i, 3];
+            }
+
+            // Рисуем отраженную фигуру с учетом текущего сдвига
+            Draw_Kv();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+          
+            // Матрица отражения относительно начала координат
+            float[,] reflectOrigin = new float[4, 4]
+            {
+                { -1, 0, 0, 0 },
+                { 0, -1, 0, 0 },
+                { 0, 0, 1, 0 },
+                { 0, 0, 0, 1 }
+            };
+
+            // Применяем отражение к текущим координатам
+            float[,] temp = Multiply_matr(kv, reflectOrigin);
+
+            // Обновляем текущие координаты фигуры
+            for (int i = 0; i < 8; i++)
+            {
+                kv[i, 0] = temp[i, 0];
+                kv[i, 1] = temp[i, 1];
+                kv[i, 2] = temp[i, 2];
+                kv[i, 3] = temp[i, 3];
+            }
+
+            // Рисуем отраженную фигуру с учетом текущего сдвига
+            Draw_Kv();
+        }
+
+        //Очистка PictureBox1
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ClearDrawing();
+        }
+
+        private void button4_Click(object sender, EventArgs e) // Поворот вокруг X
+        {
+            rotationAngleX += 0.1;
+            Draw_Kv();
+        }
+
+        private void button5_Click(object sender, EventArgs e) // Поворот вокруг Z
+        {
+            rotationAngleZ += 0.1;
+            Draw_Kv();
+        }
+
+        private void button16_Click(object sender, EventArgs e) // Поворот вокруг Y (влево)
+        {
+            rotationAngleY -= 0.1;
+            Draw_Kv();
+        }
+
+        private void button15_Click(object sender, EventArgs e) // Поворот вокруг Y (вправо)
+        {
+            rotationAngleY += 0.1;
+            Draw_Kv();
+        }
+
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            k = pictureBox1.Width / 2;
+            l = pictureBox1.Height / 2;
+            ResetFigure(); // Возвращаем фигуру в исходное положение и центр
+        }
+        private float[,] CreateRotationXMatrix(double angle)
+        {
+            float cos = (float)Math.Cos(angle);
+            float sin = (float)Math.Sin(angle);
+
+            return new float[4, 4]
+            {
+        { 1, 0,    0,     0 },
+        { 0, cos, -sin,   0 },
+        { 0, sin,  cos,   0 },
+        { 0, 0,    0,     1 }
+            };
+        }
+
+        // Матрица поворота вокруг оси Y
+        private float[,] CreateRotationYMatrix(double angle)
+        {
+            float cos = (float)Math.Cos(angle);
+            float sin = (float)Math.Sin(angle);
+
+            return new float[4, 4]
+            {
+        { cos,  0, sin, 0 },
+        { 0,    1, 0,   0 },
+        { -sin, 0, cos, 0 },
+        { 0,    0, 0,   1 }
+            };
+        }
+
+        // Матрица поворота вокруг оси Z
+        private float[,] CreateRotationZMatrix(double angle)
+        {
+            float cos = (float)Math.Cos(angle);
+            float sin = (float)Math.Sin(angle);
+
+            return new float[4, 4]
+            {
+        { cos, -sin, 0, 0 },
+        { sin,  cos, 0, 0 },
+        { 0,    0,   1, 0 },
+        { 0,    0,   0, 1 }
+            };
+        }
+    }
+}
